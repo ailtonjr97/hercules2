ff0d90afbaa3132e7a61b04d86fe4355bf1454f1
root <root@intranet>
2024-06-10 08:51:52 -0300

    terminado modulo orc

--- src/components/comercial/ComercialOrcamentos.vue
+++ src/components/comercial/ComercialOrcamentos.vue
@@ -4,7 +4,6 @@
   <div style="overflow: hidden; padding: 0.5%;">
     <table-top :resultados="resultados">
       <template v-slot:tableButtons>
-        <h6>Em desenvolvimento</h6>
         <button class="button-8 mb-2" @click="refresh()">Atualizar</button>
       </template>
     </table-top>
@@ -65,7 +64,7 @@
             <form-span :span="'Número'" :type="'text'" v-model="orcamento.CJ_NUM" readonly></form-span>
           </div>
           <div class="col">
-            <form-span :span="'DT Emissão'" :type="'text'" v-model="orcamento.CJ_EMISSAO" readonly></form-span>
+            <form-span :span="'DT Emissão'" :type="'date'" v-model="orcamento.CJ_EMISSAO" readonly></form-span>
           </div>
           <div class="col d-flex justify-content-evenly">
             <form-span :span="'Cliente'" :type="'text'" v-model="orcamento.CJ_CLIENTE" readonly></form-span>
@@ -120,8 +119,175 @@
             <form-span :span="'UF Cliente'" :type="'text'" v-model="orcamento.CJ_XESTADO" readonly></form-span>
           </div>
         </div>
+        <div class="row mt-2"  v-if="optionsOrcamentos">
+          <div class="col-sm-2">
+            <form-span :span="'PEDIDO KORP'" :type="'text'" v-model="orcamento.CJ_XPVKORP" readonly></form-span>
+          </div>
+          <div class="col-sm-2">
+            <span-select :span="'Tipo Cliente'" :options="tipoCliente" v-model="orcamento.CJ_TIPOCLI"></span-select>
+          </div>
+          <div class="col-md-4">
+            <form-span :span="'DESC TABELA'" :type="'text'" readonly></form-span>
+          </div>
+        </div>
+        <div class="row mt-2"  v-if="optionsOrcamentos">
+          <div class="col-md-4">
+            <form-span :span="'Nome Vend'" :type="'text'" v-model="orcamento.A3_NOME" readonly></form-span>
+          </div>
+          <div class="col-md-4">
+            <form-span :span="'Frete + Imp.'" :type="'text'" v-model="orcamento.CJ_XFREIMP" readonly></form-span>
+          </div>
+          <div class="col-md-4">
+            <form-span :span="'Dt. Validade'" :type="'date'" v-model="orcamento.CJ_VALIDA" readonly></form-span>
+          </div>
+        </div>
+        <div class="row mt-2"  v-if="optionsOrcamentos">
+          <span-textarea :span="'Observacao'" :altura="50" v-model="orcamento.CJ_XOBS"></span-textarea>
+        </div>
+        <div class="row mt-2"  v-if="optionsOutros">
+          <div class="col-sm-2">
+            <form-span :span="'Prazo Real'" :type="'date'" v-model="orcamento.CJ_XENTREG"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Frete Cot.'" :type="'text'" v-model="orcamento.CJ_CST_FTS" readonly></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Vlr. Frete'" :type="'text'" v-model="orcamento.CJ_XFREMA" readonly></form-span>
+          </div>
+          <div class="col-sm-3 d-flex justify-content-evenly">
+            <form-span :span="'Transp. Simu'" :type="'text'" v-model="orcamento.CJ_XTRANSP" readonly></form-span>
+            <button style="margin-left: 1%;" class="button-8" @click="openModalPadrao('Transportadora', `/consulta/transportadora?filial=${orcamento.CJ_FILIAL}&codigo=${orcamento.CJ_XTRANSP}`, ['Código', 'Nome'])">
+              <i style="font-size: 16px;" class="fas fa-search"></i>
+            </button>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Simu. Frete'" :type="'text'" v-model="orcamento.CJ_XFRESIM" readonly></form-span>
+          </div>
+        </div>
+        <div class="row mt-2"  v-if="optionsOutros">
+          <div class="col-sm-2">
+            <form-span :span="'Desconto 1'" :type="'text'" v-model="orcamento.CJ_DESC1"></form-span>
+          </div>
+          <div class="col-sm-3">
+            <form-span :span="'Cotação/Ped.'" :type="'text'" v-model="orcamento.CJ_COTCLI"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Frete'" :type="'text'" v-model="orcamento.CJ_FRETE"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Seguro'" :type="'text'" v-model="orcamento.CJ_SEGURO"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Despesa'" :type="'text'" v-model="orcamento.CJ_DESPESA"></form-span>
+          </div>
+        </div>
+        <div class="row mt-2"  v-if="optionsOutros">
+          <div class="col-sm-2">
+            <form-span :span="'Frete Auton.'" :type="'text'" v-model="orcamento.CJ_FRETAUT"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Moeda'" :type="'text'" v-model="orcamento.CJ_MOEDA"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Filial Venda'" :type="'text'"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Filial Entr.'" :type="'text'"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <span-select :span="'Carga'" :options="tipoCarga" v-model="orcamento.CJ_TPCARGA"></span-select>
+          </div>
+        </div>
+        <div class="row mt-2"  v-if="optionsOutros">
+          <div class="col-sm-2">
+            <form-span :span="'Indenizacao'"  :type="'text'" v-model="orcamento.CJ_DESCONT"></form-span>
+          </div>
+          <div class="col-sm-3">
+            <form-span :span="'Log usuario'"  :type="'text'" v-model="orcamento.CJ_LOGUSL"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Usuário Incl'" :type="'text'" v-model="orcamento.CJ_XUSRINC"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Percentual'"   :type="'text'" v-model="orcamento.CJ_XPERCEN"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Proposta No.'" :type="'text'" v-model="orcamento.CJ_PROPOST"></form-span>
+          </div>
+        </div>
+        <div class="row mt-2"  v-if="optionsOutros">
+          <div class="col-sm-2">
+            <form-span :span="'Val. Percent'"  :type="'text'" v-model="orcamento.CJ_XVALPER"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Forma Pgto.'"  :type="'text'" v-model="orcamento.CJ_XFORMA"></form-span>
+          </div>
+          <div class="col-md-5">
+            <form-span :span="'Mot. Rejeita'"  :type="'text'" v-model="orcamento.CJ_XMOTREJ"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Oportunidade'"  :type="'text'" v-model="orcamento.CJ_NROPOR"></form-span>
+          </div>
+        </div>
+        <div class="row mt-2"  v-if="optionsOutros">
+          <div class="col-sm-3 d-flex justify-content-evenly">
+            <form-span :span="'Vendedor 2'"  :type="'text'" v-model="orcamento.CJ_XVEND2"></form-span>
+            <button style="margin-left: 1%;" class="button-8" @click="openModalPadrao('Vendedor', `/consulta/vendedor?filial=${orcamento.CJ_FILIAL}&codigo=${orcamento.CJ_XVEND2}`, ['Código', 'Nome'])">
+              <i style="font-size: 16px;" class="fas fa-search"></i>
+            </button>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Usuario Lib.'"  :type="'text'" v-model="orcamento.CJ_XUSRLIB"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Revisao Op.'"  :type="'text'" v-model="orcamento.CJ_REVISA"></form-span>
+          </div>
+          <div class="col-sm-3 d-flex justify-content-evenly">
+            <form-span :span="'Vendedor 3'"  :type="'text'" v-model="orcamento.CJ_XVEND3"></form-span>
+            <button style="margin-left: 1%;" class="button-8" @click="openModalPadrao('Vendedor', `/consulta/vendedor?filial=${orcamento.CJ_FILIAL}&codigo=${orcamento.CJ_XVEND3}`, ['Código', 'Nome'])">
+              <i style="font-size: 16px;" class="fas fa-search"></i>
+            </button>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Taxa Moeda'"  :type="'text'" v-model="orcamento.CJ_TXMOEDA"></form-span>
+          </div>
+        </div>
+        <div class="row mt-2"  v-if="optionsOutros">
+          <div class="col-sm-3 d-flex justify-content-evenly">
+            <form-span :span="'Vendedor 4'"   :type="'text'" v-model="orcamento.CJ_XVEND4"></form-span>
+            <button style="margin-left: 1%;" class="button-8" @click="openModalPadrao('Vendedor', `/consulta/vendedor?filial=${orcamento.CJ_FILIAL}&codigo=${orcamento.CJ_XVEND4}`, ['Código', 'Nome'])">
+              <i style="font-size: 16px;" class="fas fa-search"></i>
+            </button>
+          </div>
+          <div class="col-sm-3">
+            <span-select :span="'Presença Com'" :options="indPres" v-model="orcamento.CJ_INDPRES"></span-select>
+          </div>
+          <div class="col-sm-2 d-flex justify-content-evenly">
+            <form-span :span="'Vendedor 5'"   :type="'text'" v-model="orcamento.CJ_XVEND5"></form-span>
+            <button style="margin-left: 1%;" class="button-8" @click="openModalPadrao('Vendedor', `/consulta/vendedor?filial=${orcamento.CJ_FILIAL}&codigo=${orcamento.CJ_XVEND5}`, ['Código', 'Nome'])">
+              <i style="font-size: 16px;" class="fas fa-search"></i>
+            </button>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Cód. Interme'" :type="'text'" v-model="orcamento.CJ_CODA1U"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Perc. Adian.'" :type="'text'" v-model="orcamento.CJ_XFINPCD"></form-span>
+          </div>
+        </div>
+        <div class="row mt-2"  v-if="optionsOutros">
+          <div class="col-sm-2">
+            <form-span :span="'Prospect'"  :type="'text'" v-model="orcamento.CJ_PROSPE"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Loja Prosp.'"  :type="'text'" v-model="orcamento.CJ_LOJPRO"></form-span>
+          </div>
+          <div class="col-sm-2">
+            <form-span :span="'Vlr. Adian'"  :type="'text'" v-model="orcamento.CJ_XFINVLD"></form-span>
+          </div>
+        </div>
         <div class="row mt-2">
-          <loading v-if="carregandoItemsOrc"></loading>
+          <loading v-if="carregandoItemsOrc && optionsOrcamentos"></loading>
         </div>
         <div v-if="!carregandoItemsOrc && optionsOrcamentos" class="table-wrapper table-responsive table-striped mt-4">
           <table class="fl-table" id="myTable">
@@ -158,8 +324,8 @@
 
   <modal v-if="modalPadrao" :title="modalPadraoTitle">
     <template v-slot:body>
-      <loading v-if="carregandoinfo"></loading>
-      <div v-if="!carregandoinfo">
+      <loading v-if="carregandoInfoModal"></loading>
+      <div v-if="!carregandoInfoModal">
         <div class="row">
           <div v-for="(item, index) in modalPadraoItem" class="col-md-3 mt-2">
             <form-span :span="modalPadraoDescritivos[index]" :type="'text'" v-model="computedModalPadraoItems[index]" readonly></form-span>
@@ -167,7 +333,7 @@
         </div>
       </div>
     </template>
-    <template v-slot:buttons v-if="!carregandoinfo">
+    <template v-slot:buttons v-if="!carregandoInfoModal">
       <button class="button-8 mt-2" @click="modalPadrao = false">Fechar</button>
     </template>
   </modal>
@@ -188,6 +354,7 @@ import TextareaFloating from '../ui/TextareaFloating.vue';
 import SelectFloating from '../ui/SelectFloating.vue';
 import Popup from '../ui/Popup.vue';
 import SpanSelect from '../ui/spanSelect.vue';
+import SpanTextarea from '../ui/spanTextarea.vue';
 
 const config = getAuthConfig();
 
@@ -203,14 +370,37 @@ export default {
     TextareaFloating,
     SelectFloating,
     FormSpan,
-    SpanSelect
+    SpanSelect,
+    SpanTextarea
   },
   data() {
     return {
+      carregandoInfoModal: false,
       modalPadraoDescritivos: null,
       modalPadraoTitle: '',
       modalPadraoItem: null,
       modalPadrao: false,
+      //0=Não aplica;1=Presencial;2=Não Presencial,Net;3=Não Presencial,Teleatend.;5=Presencial,Fora do Estab.;9=Não Presencial,Outros  
+      indPres: [                                                                              
+        { valor: "0", descri: '0 - Não aplica' },
+        { valor: "1", descri: '1 - Presencial' },
+        { valor: "2", descri: '2 - Não Presencial,Net' },
+        { valor: "3", descri: '3 - Não Presencial,Teleatend.' },
+        { valor: "5", descri: '5 - Presencial,Fora do Estab.' },
+        { valor: "9", descri: '9 - Não Presencial,Outros ' },
+      ],
+      tipoCarga: [                                                                              
+        { valor: "1", descri: '1 - Utiliza' },
+        { valor: "2", descri: '2 - Nao utiliza' },
+        { valor: "3", descri: '3 - Utiliza sem unitizacao  ' },
+      ],
+      tipoCliente: [
+        { valor: "F", descri: 'F - Cons.Final' },
+        { valor: "L", descri: 'L - Prod.Rural' },
+        { valor: "R", descri: 'R - Revendedor' },
+        { valor: "S", descri: 'S - Solidario' },
+        { valor: "X", descri: 'X - Exportacao/Importacao'},
+      ],
       tpLiberacao: [
         { valor: "1", descri: '1 - Libera por Item' },
         { valor: "2", descri: '2 - Libera por Pedido' },
@@ -270,14 +460,14 @@ export default {
         this.modalPadraoDescritivos = descritivos;
         this.modalPadraoTitle = title;
         this.modalPadrao = true;
-        this.carregandoinfo = true;
+        this.carregandoInfoModal = true;
         const response = await axios.get(`${import.meta.env.VITE_BACKEND_IP}${api}`, config);
         this.modalPadraoItem = response.data;
-        this.carregandoinfo = false;
+        this.carregandoInfoModal = false;
       } catch (error) {
-        this.carregandoinfo = false;
+        this.carregandoInfoModal = false;
         if (error.response.status == 404) {
-          alert('Não encontrado nenhum resultado.');
+          alert('Nenhum resultado encontrado.');
         } else {
           alert('Falha ao executar ação. Tente novamente mais tarde');
         }
