88c6afd29e39223f81d754dcb1c6cc5ca97396e3
root <root@intranet>
2024-09-25 08:42:10 -0300

    trocada hash icones

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -16,7 +16,7 @@
 
             <div class="dropdown" style="margin-left: 15%;">
                 <i class="fa-solid fa-comments-dollar" style="color: white; font-size: 20px; margin-top: 40%; cursor: pointer;" data-bs-toggle="dropdown"></i>
-                <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Comercial</p>
+                <!-- <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Comercial</p> -->
                 <div class="dropdown-menu">
                     <h6 class="dropdown-header">Comercial</h6>
                     <router-link to="/comercial/cotacao-de-frete" class="nav-link" style="padding: 0 8px;">Cotação de Frete</router-link>
@@ -27,7 +27,7 @@
 
             <div class="dropdown" style="margin-left: 20%;">
                 <i class="fa-solid fa-dollar" style="color: white; font-size: 20px; margin-top: 80%; cursor: pointer;" data-bs-toggle="dropdown"></i>
-                <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Financeiro</p>
+                <!-- <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Financeiro</p> -->
                 <div class="dropdown-menu">
                     <h6 class="dropdown-header">Financeiro</h6>
                     <router-link to="/financeiro/analise-de-credito" class="nav-link" style="padding: 0 8px;">Análise de Crédito</router-link>
@@ -40,7 +40,7 @@
 
             <div class="dropdown" style="margin-left: 20%;">
                 <i class="fa-solid fa-circle-check" style="color: white; font-size: 21px; margin-top: 48%; cursor: pointer;" data-bs-toggle="dropdown"></i>
-                <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Qualidade</p>
+                <!-- <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Qualidade</p> -->
                 <div class="dropdown-menu">
                     <h6 class="dropdown-header">Qualidade</h6>
                     <router-link to="/qualidade/for-edp-25" class="nav-link" style="padding: 0 8px;">FOR-EDP-25</router-link>
@@ -49,7 +49,7 @@
 
             <div class="dropdown" style="margin-left: 20%;">
                 <i class="fa-solid fa-truck" style="color: white; font-size: 21px; margin-top: 38%; cursor: pointer;" data-bs-toggle="dropdown"></i>
-                <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Logística</p>
+                <!-- <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Logística</p> -->
                 <div class="dropdown-menu">
                     <h6 class="dropdown-header">Logística</h6>
                     <router-link to="/logistica/produtos" class="nav-link" style="padding: 0 8px;">Produtos</router-link>
@@ -58,7 +58,7 @@
 
             <div class="dropdown" style="margin-left: 20%;">
                 <i class="fa-solid fa-database" style="color: white; font-size: 21px; margin-top: 55%; cursor: pointer;" data-bs-toggle="dropdown"></i>
-                <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Tabelas</p>
+                <!-- <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Tabelas</p> -->
                 <div class="dropdown-menu">
                     <h6 class="dropdown-header">Banco de dados</h6>
                     <router-link to="/tabelas" class="nav-link" style="padding: 0 8px;">Tabelas</router-link>
@@ -67,7 +67,7 @@
 
             <div class="dropdown" style="margin-left: 20%;">
                 <i class="fa-solid fa-chart-pie" style="color: white; font-size: 21px; margin-top: 45%; cursor: pointer;" data-bs-toggle="dropdown"></i>
-                <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Gráficos</p>
+                <!-- <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Gráficos</p> -->
                 <div class="dropdown-menu">
                     <h6 class="dropdown-header">Gráficos</h6>
                     <router-link to="/graficos-orcamentos" class="nav-link" style="padding: 0 8px;">Orçamentos</router-link>
@@ -77,7 +77,7 @@
 
             <div class="dropdown" style="margin-left: 20%;">
                 <i class="fa-solid fa-user" style="color: white; font-size: 20px; margin-top: 60%; cursor: pointer;" data-bs-toggle="dropdown"></i>
-                <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Usuários</p>
+                <!-- <p data-bs-toggle="dropdown" style="margin-top: 10%; color: white;">Usuários</p> -->
                 <div class="dropdown-menu">
                     <h6 class="dropdown-header">{{ name }}</h6>
                     <router-link to="/usuarios" class="nav-link" style="padding: 0 8px;" :style="[isAdmin != 0 ? {'display': 'block'} : {'display': 'none'}]">Usuários</router-link>
